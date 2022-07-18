133dc3512216acd72d8582163e1a27af70908e2a
Diego Salas <diesalasmart@gmail.com>
2022-07-17 21:49:57 -0500

    add skill icons

--- pages/skills/back-skills.js
+++ pages/skills/back-skills.js
@@ -4,10 +4,21 @@ import { Image } from '@chakra-ui/react'
 
 const skills = [
   {
-    src: '/icons/py.png',
-    alt: 'Python'
+    src: '/icons/spring.png',
+    alt: 'Spring'
   },
-
+  {
+    src: '/icons/node.png',
+    alt: 'NodeJS'
+  },  
+  {
+    src: '/icons/express.png',
+    alt: 'Express'
+  },
+  {
+    src: '/icons/python.png',
+    alt: 'Python'
+  }
 ]
 
 const BackSkills = () => {
@@ -18,7 +29,12 @@ const BackSkills = () => {
           <motion.div
             key={id}
             className="item-carousel"
-            style={{ minHeigth: '200px', minWidth: '200px', maxHeigth: '200px', maxWidth: '200px'}}
+            style={{
+              minHeigth: '200px',
+              minWidth: '200px',
+              maxHeigth: '200px',
+              maxWidth: '200px'
+            }}
           >
             <Image src={s.src} alt={s.alt} w="100%" h="100%" />
           </motion.div>
--- pages/skills/front-skills.js
+++ pages/skills/front-skills.js
@@ -4,14 +4,6 @@ import { SkillsCarousel } from '../../components/carousel'
 
 const skills = [
   {
-    src: '/icons/figma.png',
-    alt: 'Figma'
-  },
-  {
-    src: '/icons/git.png',
-    alt: 'Git'
-  },
-  {
     src: '/icons/react.png',
     alt: 'React'
   },
@@ -21,7 +13,15 @@ const skills = [
   },
   {
     src: '/icons/css.png',
-    alt: 'Css'
+    alt: 'CSS'
+  },
+  {
+    src: '/icons/js.png',
+    alt: 'JS'
+  },
+  {
+    src: '/icons/figma.png',
+    alt: 'Figma'
   }
 ]
 
@@ -33,7 +33,12 @@ const FrontSkills = () => {
           <motion.div
             key={id}
             className="item-carousel"
-            style={{ minHeigth: '200px', minWidth: '200px', maxHeigth: '200px', maxWidth: '200px'}}
+            style={{
+              minHeigth: '200px',
+              minWidth: '200px',
+              maxHeigth: '200px',
+              maxWidth: '200px'
+            }}
           >
             <Image src={s.src} alt={s.alt} w="100%" h="100%" />
           </motion.div>
