84b97decb77438814d2904a2483bd503d4878ea0
Diego Salas <diesalasmart@gmail.com>
2022-07-20 00:58:34 -0500

    update meta tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,13 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8} sx={{ overflowX: 'hidden' }}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Diego's homepage" />
+        <meta name="author" conteent="Diego Salas" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Diego Salas" />
+        <meta name="og:title" content="Diego Salas" />
+        <meta property="og:type" content="website" />
         <title>Diego Salas - Homepage</title>
       </Head>
       <Navbar path={router.asPath} />
